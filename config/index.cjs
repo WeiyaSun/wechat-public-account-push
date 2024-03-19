fc8855736e924e5fe2ad3ff2b012793ba1e32fc7
WeiyaSun <163874758+WeiyaSun@users.noreply.github.com>
2024-03-19 15:54:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '臭屁雷雷',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oxVDq6Be-p7twuxiOoZCZoNcPc9o	',
+      id: 'oxVDq6Be-p7twuxiOoZCZoNcPc9o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '7WEiT5nrXL0PkzPeBA5Qvn2I8wstBdTrDYEUxpGvbO4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
