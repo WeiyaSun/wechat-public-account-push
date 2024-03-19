808b2826540434f1450b1eca5bb0b7a427ac5a4d
WeiyaSun <163874758+WeiyaSun@users.noreply.github.com>
2024-03-19 16:07:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oxVDq6Be-p7twuxiOoZCZoNcPc9o	',
+      id: 'oxVDq6Be-p7twuxiOoZCZoNcPc9o',
     }
   ],
 
