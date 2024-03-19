7ded5fa88c6b16240cf2023962f7f1724d8e527a
WeiyaSun <163874758+WeiyaSun@users.noreply.github.com>
2024-03-19 16:03:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oxVDq6Be-p7twuxiOoZCZoNcPc9o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '7WEiT5nrXL0PkzPeBA5Qvn2I8wstBdTrDYEUxpGvbO4',
+      useTemplateId: 'AAt3vErMuFgO0zAc8sj60Z49lbTpVVavcv7Qfhz9Zeg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-27',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '7WEiT5nrXL0PkzPeBA5Qvn2I8wstBdTrDYEUxpGvbO4',
+  CALLBACK_TEMPLATE_ID: 'AAt3vErMuFgO0zAc8sj60Z49lbTpVVavcv7Qfhz9Zeg',
 
   CALLBACK_USERS: [
     {
